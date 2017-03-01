38fd575c224ac398ce6b6e317510abc924e803f8
Jean-Baptiste Aniel <rhanb@Jean-Baptistes-MacBook-Pro.local>
2017-03-01 17:31:38 +0100

    implementing Scrollify

--- js/script.js
+++ js/script.js
@@ -12,27 +12,48 @@ function collapseNavbar() {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 }
-
-$(window).scroll(collapseNavbar);
-$(document).ready(collapseNavbar);
-
 // Scrolling page -> jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+$(function (collapseNavbar) {
+    $(window).scroll(collapseNavbar);
+    // Responsive Menu
+    $('.navbar-collapse ul li a').click(function () {
+        if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+            $('.navbar-toggle:visible').click();
+        }
+    });
+    $.scrollify({
+        section: ".section",
+        scrollbars: false,
+        before: function (i, sections) {
+            var ref = sections[i].attr("data-section-name");
+            $(".pagination .active").removeClass("active");
+            $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+        },
+        afterRender: function () {
+            var pagination = "<ul class=\"pagination\">";
+            var activeClass = "";
+            $(".section").each(function (i) {
+                activeClass = "";
+                if (i === 0) {
+                    activeClass = "active";
+                }
+                pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+            });
+            pagination += "</ul>";
+            $("#home").append(pagination);
+        }
+    });
+    $('a.page-scroll').bind('click', function (event) {
         event.preventDefault();
+        $.scrollify.next();
+    });
+    $(".pagination a").on("click", function(){
+        console.log('pagination clicked');
+        $.scrollify.move();
     });
 });
 
-// Responsive Menu
-$('.navbar-collapse ul li a').click(function() {
-  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
-    $('.navbar-toggle:visible').click();
-  }
-});
+
 
 
 
